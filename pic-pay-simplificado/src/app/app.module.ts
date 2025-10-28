f6d552b140b422b6d1e652012bde482d5436eeec
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-28 15:34:49 -0300

    Configurações de app
    Configurações de app

---
+++ pic-pay-simplificado/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransacaoModule } from 'src/transacao/transacao.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'pic_pay_simplificado',
+    synchronize: true,
+    autoLoadEntities: true
+  }),UsuariosModule, TransacaoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
