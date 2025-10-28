f667c9e12c03fe94866a00875603b813ca37b711
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-28 15:36:08 -0300

    Create transacao.controller.ts
    criacao do controller de transacao

---
+++ pic-pay-simplificado/src/transacao/transacao.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
+import { TransacaoDto } from "./dto/transacao.dto";
+import type { Response } from "express";
+import { TransacaoService } from "./transacao.service";
+
+@Controller()
+export class TransacaoController{
+
+    constructor(private readonly transacaoService: TransacaoService){
+
+    }
+
+    @Post('/transacao')
+        async postTransacao(@Body() transacao: TransacaoDto, @Res() res: Response): Promise<Response>{
+            try{
+                await this.transacaoService.postTransacao(transacao)
+                return res.status(HttpStatus.OK).send("Transação realizada com sucesso")
+            }catch(erro){
+                return res.status(HttpStatus.BAD_REQUEST).send("Erro na transacao\n"+ erro)
+            }
+        }
+}
