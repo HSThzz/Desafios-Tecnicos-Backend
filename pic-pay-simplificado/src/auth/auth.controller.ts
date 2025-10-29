13319fd27addab7a4a8afec118e816276a150386
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-29 15:19:14 -0300

    Update auth.controller.ts
    documentando controller de autenticação

--- pic-pay-simplificado/src/auth/auth.controller.ts
+++ pic-pay-simplificado/src/auth/auth.controller.ts
@@ -2,12 +2,24 @@ import { Body, Controller, HttpStatus, Post, Res, UnauthorizedException } from "
 import { LoginDto } from "./dto/login.dto";
 import { AuthService } from "./auth.service";
 import type{ Response } from "express";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller()
 export class AuthController{
 
     constructor(private readonly authService: AuthService){}
 
+    
+    @ApiOperation({summary: 'fornece o token de autenticação para login'})
+    @ApiBody({schema: {
+        example: {
+	        "email": "teste2@gmail.com",
+	        "senha": "senha123"
+        }
+    }})
+    @ApiResponse({status: 200, description: 'Login realizado com sucesso'})
+    @ApiResponse({status: 401, description: 'Senha incorreta'})
+    @ApiResponse({status: 404, description: 'Usuario nao encontrado'})
     @Post("/login")
     async postLogin(@Body() loginDto: LoginDto, @Res()res: Response): Promise<Response>{
         try{
