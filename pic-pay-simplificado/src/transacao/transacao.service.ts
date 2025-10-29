e4a2c718c85d37bda7b8471877150c33af6ccec9
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-29 12:14:38 -0300

    Update transacao.service.ts
    criando metodo de salvar transacao no banco de dados

--- pic-pay-simplificado/src/transacao/transacao.service.ts
+++ pic-pay-simplificado/src/transacao/transacao.service.ts
@@ -1,17 +1,21 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { BadRequestException, GatewayTimeoutException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Usuarios } from "src/usuarios/entity/usuarios.entity";
-import { Repository } from "typeorm";
+import { EntityManager, Repository } from "typeorm";
 import { TransacaoDto } from "./dto/transacao.dto";
 import { TipoUsuario } from "src/usuarios/usuarios.enum";
 import axios from "axios";
+import { Transacao } from "./entity/transacao.entity";
 
 @Injectable()
 export class TransacaoService{
 
     constructor(
         @InjectRepository(Usuarios)
-        private usuariosRepository: Repository<Usuarios>
+        private usuariosRepository: Repository<Usuarios>,
+
+        @InjectRepository(Transacao)
+        private transacaoRepository: Repository<Transacao>
     ){
 
     }
@@ -38,8 +42,32 @@ export class TransacaoService{
         return true
     }
 
-    async postTransacao({pagador: idPagador, recebedor: idRecebedor, valor}: TransacaoDto): Promise<void>{
+    async verificaAutenticacao(){
+            try{
+                const resposta = await axios.get("https://util.devi.tools/api/v2/authorize")
+                if (resposta.data.status === "fail" || resposta.data.data?.authorization === false) {
+                    throw new UnauthorizedException("Transação não autorizada pelo serviço externo");
+        }
+            }catch(erro){
+                throw new UnauthorizedException("Erro ao validar transação")
+            }     
+    }
 
+    async enviaNotificacao(){
+        try{
+            await axios.post("https://util.devi.tools/api/v1/notify")
+        }catch(erro){
+            throw new GatewayTimeoutException("Falha ao enviar notificação")
+        }
+    }
+    
+
+    async realizaTransacao(transacaoDto: TransacaoDto): Promise<void>{
+
+            const idPagador = transacaoDto.pagador
+            const idRecebedor = transacaoDto.recebedor
+            const valor = transacaoDto.valor
+            
             if(idPagador === idRecebedor)
                 throw new BadRequestException("Nao é permitido transferir para si mesmo");
     
@@ -51,19 +79,32 @@ export class TransacaoService{
 
             await this.usuariosRepository.manager.transaction(async (manager)=>{
                 
-                try{
-                    const resposta = await axios.get("https://util.devi.tools/api/v2/authorize")
-                }catch(erro){
-                    throw new UnauthorizedException("Erro ao validar transação")
-                }
-            
+                
+                await this.verificaAutenticacao()
                 
                 pagador.saldo -= valor
                 recebedor.saldo += valor
 
                 await manager.save(pagador);
                 await manager.save(recebedor);
+                await this.salvaTransacao(transacaoDto, manager)
+                await this.enviaNotificacao()
+                
+
             })
-        } 
+        }
+    async salvaTransacao(transacaoDto: TransacaoDto, manager: EntityManager): Promise<void>{
+            const idPagador = transacaoDto.pagador
+            const idRecebedor = transacaoDto.recebedor
+
+            const transacao = manager.create(Transacao, {
+                recebedor: idRecebedor,
+                valor: transacaoDto.valor,
+                tipo_transacao: transacaoDto.tipo_transacao,
+                usuario: {id: idPagador}
+            })
+            await manager.save(transacao)
+        
+    } 
 
 }
