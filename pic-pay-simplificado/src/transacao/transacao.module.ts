ba31568117a97d9b4c4f6455eb7d2eb011a00d27
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-29 12:14:19 -0300

    Update transacao.module.ts
    adicionando transacao ao typeorm

--- pic-pay-simplificado/src/transacao/transacao.module.ts
+++ pic-pay-simplificado/src/transacao/transacao.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Usuarios } from "src/usuarios/entity/usuarios.entity";
 import { TransacaoController } from "./transacao.controller";
 import { TransacaoService } from "./transacao.service";
+import { Transacao } from "./entity/transacao.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Usuarios])],
+    imports: [TypeOrmModule.forFeature([Usuarios, Transacao]), Transacao],
     controllers: [TransacaoController],
     providers: [TransacaoService]
 })
