2eb94f835444bb81bb4994e376f39ab6ae2cf315
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-10-29 13:20:43 -0300

    Update auth.module.ts
    configurando jwt

--- pic-pay-simplificado/src/auth/auth.module.ts
+++ pic-pay-simplificado/src/auth/auth.module.ts
@@ -3,11 +3,18 @@ import { Usuarios } from "src/usuarios/entity/usuarios.entity";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./auth.constants";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Usuarios])],
+    imports: [TypeOrmModule.forFeature([Usuarios]), JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: {expiresIn: '120s'}
+    })],
     controllers: [AuthController],
     providers: [AuthService],
+    exports: [AuthService]
 })
 export class AuthModule{
 
