c817393125ce4efc13fea6e93a5fd12c6f2f0084
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-11-06 14:42:23 -0300

    Update documento.controller.ts
    criando metodo de busca por documentos

--- api_beneficiarios/src/documento/documento.controller.ts
+++ api_beneficiarios/src/documento/documento.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Param, Post, Res } from "@nestjs/common";
 import { DocumentoService } from "./documento.service";
 import { DocumentoDto } from "./dto/documento.dto";
 import type { Response } from "express";
@@ -13,11 +13,21 @@ export class DocumentoController{
 
     }
 
-    @Post()
+    @Post('/post/documentos')
     async novoDocumento(@Body() body: DocumentoDto, @Res() res: Response): Promise<Response>{
         try{
             await this.documentoService.novoDocumento(body)
             return res.status(HttpStatus.OK).send("Documento criado com sucesso")
+        }catch(erro){
+            return res.status(HttpStatus.BAD_REQUEST).send(erro.message)
+        }
+    }
+
+    @Get('/get/documentos')
+    async buscaDocumentos(@Param('id') id: string, @Res() res: Response): Promise<Response>{
+        try{
+            const documentos = await this.documentoService.buscaDocumentos(Number(id))
+            return res.status(HttpStatus.OK).json(documentos)
         }catch(erro){
             return res.status(HttpStatus.BAD_REQUEST).send(erro.message)
         }
