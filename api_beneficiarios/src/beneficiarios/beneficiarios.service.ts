8aad7c689d8ab35cdf522cda3c5182c190504074
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-11-06 15:26:11 -0300

    Create beneficiarios.service.ts
    implementando metodos na service de beneficiarios

---
+++ api_beneficiarios/src/beneficiarios/beneficiarios.service.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Beneficiario } from "./entity/beneficiario.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { BeneficiarioDto } from "./dto/beneficiario.dto";
+
+@Injectable()
+export class BeneficiariosService{
+
+    constructor(
+        @InjectRepository(Beneficiario)
+        private beneficiariosRepository: Repository<Beneficiario>
+    ){
+
+    }
+
+    async novoBeneficiario(body: BeneficiarioDto): Promise<void>{
+
+        const beneficiario = await this.beneficiariosRepository.findOne({
+            where: {
+                telefone: body.telefone
+            }
+        })
+        if(beneficiario)
+           throw new BadRequestException("Beneficiario ja cadastrado")
+
+        const dataAtual = new Date()
+        
+        const novoBeneficiario = this.beneficiariosRepository.create({
+            nome: body.nome,
+            telefone: body.telefone,
+            dataNascimento: body.dataNascimento,
+            dataInclusao: dataAtual,
+            dataAtualizacao: dataAtual,
+            documentos: []
+        })
+
+        await this.beneficiariosRepository.save(novoBeneficiario)
+    }
+
+}
