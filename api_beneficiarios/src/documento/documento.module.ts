d4ed3f764f58be8cc2348b277b39417a1f17640a
Thiago Esmerio Fernandes <thiagoesmerio123@gmail.com>
2025-11-06 14:42:37 -0300

    Update documento.module.ts
    importando modulo de beneficiario

--- api_beneficiarios/src/documento/documento.module.ts
+++ api_beneficiarios/src/documento/documento.module.ts
@@ -3,9 +3,10 @@ import { DocumentoController } from "./documento.controller";
 import { DocumentoService } from "./documento.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Documento } from "./entity/documento.entity";
+import { Beneficiario } from "src/beneficiarios/entity/beneficiario.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Documento]), Documento],
+    imports: [TypeOrmModule.forFeature([Documento, Beneficiario]), Documento],
     controllers: [DocumentoController],
     providers: [DocumentoService]
 })
